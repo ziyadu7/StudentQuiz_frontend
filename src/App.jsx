50079eb204d70f230f0bb2a148ffd64cb46739e2
Muhammed Ziyad <muhdziyadu@gmail.com>
2023-11-07 09:22:17 +0530

    localhost/ redirect to login

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import StudentRouter from './routes/studentRouter'
 import AdminRoute from './routes/adminRoute'
 import NotFound from './pages/notFound'
+import Login from './pages/student/login'
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path='/' element = {<Login/>}/>
         <Route path='/student/*' element = {<StudentRouter/>}/>
         <Route path='/admin/*' element = {<AdminRoute/>}/>
         <Route path='/*' element = {<NotFound/>}/>
